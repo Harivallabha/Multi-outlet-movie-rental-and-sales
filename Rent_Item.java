5772dc2c0d1c133c7e7e0bcc28ca954fd5f0364b
Nikhil L <44635245+NikhilLokesh507@users.noreply.github.com>
2019-04-28 12:22:06 +0530

    Add files via upload

--- Rent_Item.java
+++ Rent_Item.java
@@ -10,10 +10,92 @@ public class Rent_Item{
     String renttxn_site_id;
     String emp_id,emp_name,emp_siteid,cost;
 
-    public static void main()
-    {
-        System.out.println("Successful");
+    public Date getRent_date() {
+        return rent_date;
     }
 
+    public String getCost() {
+        return cost;
+    }
+
+    public String getEmp_id() {
+        return emp_id;
+    }
+
+    public String getEmp_name() {
+        return emp_name;
+    }
+
+    public String getEmp_siteid() {
+        return emp_siteid;
+    }
+
+    public Date getRent_returndate() {
+        return rent_returndate;
+    }
+
+    public String getMovr_id() {
+        return movr_id;
+    }
+
+    public String getMovr_name() {
+        return movr_name;
+    }
+
+    public String getMovr_reg_siteid() {
+        return movr_reg_siteid;
+    }
+
+    public String getRenttxn_id() {
+        return renttxn_id;
+    }
+
+    public String getRenttxn_site_id() {
+        return renttxn_site_id;
+    }
+
+    public void setMovr_reg_siteid(String movr_reg_siteid) {
+        this.movr_reg_siteid = movr_reg_siteid;
+    }
+
+    public void setMovr_id(String movr_id) {
+        this.movr_id = movr_id;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public void setEmp_id(String emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
+    }
+
+    public void setEmp_siteid(String emp_siteid) {
+        this.emp_siteid = emp_siteid;
+    }
+
+    public void setMovr_name(String movr_name) {
+        this.movr_name = movr_name;
+    }
+
+    public void setRent_date(Date rent_date) {
+        this.rent_date = rent_date;
+    }
+
+    public void setRent_returndate(Date rent_returndate) {
+        this.rent_returndate = rent_returndate;
+    }
+
+    public void setRenttxn_id(String renttxn_id) {
+        this.renttxn_id = renttxn_id;
+    }
+
+    public void setRenttxn_site_id(String renttxn_site_id) {
+        this.renttxn_site_id = renttxn_site_id;
+    }
     //public void DeleteRent
 }
--- Sell_item.java
+++ Sell_item.java
@@ -9,10 +9,60 @@ public class Sell_item {
     String selltxn_site_id;
     String stk_sellingprice;
 
-    public static void main()
-    {
-        System.out.println("Successful");
+    public String getMovs_id() {
+        return movs_id;
     }
 
+    public String getMovs_name() {
+        return movs_name;
+    }
+
+    public String getMovs_reg_siteid() {
+        return movs_reg_siteid;
+    }
+
+    public Date getSelltxn_date() {
+        return selltxn_date;
+    }
+
+    public String getSelltxn_id() {
+        return selltxn_id;
+    }
+
+    public String getSelltxn_site_id() {
+        return selltxn_site_id;
+    }
+
+    public String getStk_sellingprice() {
+        return stk_sellingprice;
+    }
+
+    public void setMovs_id(String movs_id) {
+        this.movs_id = movs_id;
+    }
+
+    public void setMovs_name(String movs_name) {
+        this.movs_name = movs_name;
+    }
+
+    public void setMovs_reg_siteid(String movs_reg_siteid) {
+        this.movs_reg_siteid = movs_reg_siteid;
+    }
+
+    public void setSelltxn_date(Date selltxn_date) {
+        this.selltxn_date = selltxn_date;
+    }
+
+    public void setSelltxn_id(String selltxn_id) {
+        this.selltxn_id = selltxn_id;
+    }
+
+    public void setSelltxn_site_id(String selltxn_site_id) {
+        this.selltxn_site_id = selltxn_site_id;
+    }
+
+    public void setStk_sellingprice(String stk_sellingprice) {
+        this.stk_sellingprice = stk_sellingprice;
+    }
     //public void DeleteRent
 }
